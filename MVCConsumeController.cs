82a116168934555e1aae5c131ff01dbde4af0498
HackDeep12 <125531073+HackDeep12@users.noreply.github.com>
2023-02-17 16:37:30 +0530

    Add files via upload

---
+++ MVCConsumeController.cs
@@ -0,0 +1,132 @@
+﻿using RESTAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RESTAPI.Controllers
+{
+    public class MVCConsumeController : Controller
+    {
+        // GET: MVCConsume
+        HttpClient client = new HttpClient();
+
+        public ActionResult Index()
+        {
+            List<Employee> emp = new List<Employee>();
+            client.BaseAddress = new Uri("http://localhost:49620/api/New");
+            var response = client.GetAsync("New");
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<Employee>>();
+                display.Wait();
+                emp = display.Result;
+            }
+
+            return View(emp);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Employee emp = null;
+            client.BaseAddress = new Uri("http://localhost:49620/api/New");
+            var response = client.GetAsync("New?id="+id.ToString());
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                emp = display.Result;
+            }
+
+            return View(emp);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee e)
+        {
+            client.BaseAddress = new Uri("http://localhost:49620/api/New");
+            var response = client.PostAsJsonAsync<Employee>("New",e);
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Employee emp = null;
+            client.BaseAddress = new Uri("http://localhost:49620/api/New");
+            var response = client.GetAsync("New?id=" + id.ToString());
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                emp = display.Result;
+            }
+
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee e)
+        {
+            client.BaseAddress = new Uri("http://localhost:49620/api/New");
+            var response = client.PutAsJsonAsync<Employee>("New",e);
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Edit");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Employee emp = null;
+            client.BaseAddress = new Uri("http://localhost:49620/api/New");
+            var response = client.GetAsync("New?id=" + id.ToString());
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                emp = display.Result;
+            }
+
+            return View(emp);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            client.BaseAddress = new Uri("http://localhost:49620/api/New");
+            var response = client.DeleteAsync("New/" + id.ToString());
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View("Delete");
+        }
+    }
+}
---
+++ NewController.cs
@@ -0,0 +1,52 @@
+﻿using RESTAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RESTAPI.Controllers
+{
+    public class NewController : ApiController
+    {
+        web_api_crud_dbEntities db = new web_api_crud_dbEntities();
+
+        public IHttpActionResult GetData()
+        {
+            List<Employee> list = db.Employees.ToList();
+            return Ok(list);
+        }
+
+        public IHttpActionResult GetDataById(int id)
+        {
+            var details = db.Employees.Where(x=>x.id==id).FirstOrDefault();
+            return Ok(details);
+        }
+
+        [HttpPost]
+        public IHttpActionResult InsertEmployee(Employee e)
+        {
+            db.Employees.Add(e);
+            db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        public IHttpActionResult UpdateEmp(Employee e)
+        {
+            db.Entry(e).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteEmp(int id)
+        {
+            var emp = db.Employees.Where(x => x.id == id).FirstOrDefault();
+            db.Entry(emp).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return Ok();
+        }
+    }
+}
